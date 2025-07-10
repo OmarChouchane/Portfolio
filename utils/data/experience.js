7f90e936aea73e465503e29544ec1f96515dca83
OmarChouchane <omar.ch52831@gmail.com>
2025-07-10 16:36:02 +0100

    icon fixes

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -9,6 +9,6 @@ export const experiences = [
     id: 2,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2018 - Present)",
+    duration: "(2020 - Present)",
   },
 ];
